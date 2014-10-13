603845fe585d0142e5a11e5319721ebd5dab7415
Thushara Kasun Ranawaka <thushara35@gmail.com>
2014-10-13 17:17:53 +0530

    added run_scripts.sh

--- scripts/run_scripts.sh
+++ scripts/run_scripts.sh
@@ -18,5 +18,9 @@ echo "Starting /usr/local/bin/couchdb" >> $LOG
 /usr/local/bin/couchdb couchdb > /dev/null 2>&1 &
 echo "Ending /usr/local/bin/couchdb" >> $LOG
 
+echo "Starting /usr/local/bin/couch_apps.sh" >> $LOG
+/usr/local/bin/couch_apps.sh > /dev/null 2>&1 &
+echo "Ending /usr/local/bin/couch_apps.sh" >> $LOG
+
 echo "Ending run_script..." >> $LOG
 echo " " >> $LOG
